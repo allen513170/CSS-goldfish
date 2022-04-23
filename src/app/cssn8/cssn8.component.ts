72d552aa89cb922b543078e9dabfa95dd677f31c
Ryan <ryan.lin@hbt.com.tw>
2022-04-23 13:14:57 +0800

    DAY8

---
+++ src/app/cssn8/cssn8.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cssn8',
+  templateUrl: './cssn8.component.html',
+  styleUrls: ['./cssn8.component.css']
+})
+export class Cssn8Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
